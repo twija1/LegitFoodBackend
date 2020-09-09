ca47030bd045ae6406a2583130118f7b67320824
twija <238354@student.pwr.edu.pl>
2020-09-09 16:45:41 +0200

    Added Dish database Schema

---
+++ src/Models/Dishes/Dish.ts
@@ -0,0 +1,43 @@
+import mongoose, {Schema, Document} from "mongoose";
+
+export type DishType = {
+    name: string,
+    photo: string,
+    price: number,
+    ingredients?: string[],
+    allergens: string[],
+    description: string,
+} & Document
+
+const dishSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    photo: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    ingredients: {
+        type: [String],
+        default: []
+    },
+    allergens: {
+        type: [String],
+        validate: {
+            validator: (value: string[]) => value.length > 0,
+            message: 'You need to add allergens!'
+        },
+        required: [true, 'You need to add allergens!']
+    },
+    description: {
+        type: String,
+        required: true
+    }
+})
+
+export const Dish = mongoose.model<DishType>("Dish", dishSchema)
