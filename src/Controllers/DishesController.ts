9af8656c9f483fb5c29986341d04594af8538735
twija <238354@student.pwr.edu.pl>
2020-09-11 12:01:23 +0200

    Renamed DishController to DishesController

--- src/Controllers/DishesController.ts
+++ src/Controllers/DishesController.ts
@@ -1,16 +1,16 @@
 import express from 'express'
-import {simplifyDBDocuments} from "../lib/dbobjectsimplifier";
+import {simplifyDBDocuments} from "../lib/DBObjectSimplifier";
 import * as DishService from '../Models/Dishes/DishService'
 
-const DishController = express.Router();
+const DishesController = express.Router();
 
 const serverError = (res: any) => () => res.status(500).send({message: 'Server error'})
 
-DishController.get('/', (req, res, next) => {
+DishesController.get('/', (req, res, next) => {
     DishService.displayAllDishes()
         .then(simplifyDBDocuments)
         .then(r => res.send(r))
         .catch(serverError(res))
 });
 
-export default DishController;
+export default DishesController;
